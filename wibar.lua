260e1d53651d84ba9e73c3aec5b7b4dbec9632cd
Young <yongchihua@gmail.com>
2024-06-14 22:36:22 +0800

    添加电量小组件

--- wibar.lua
+++ wibar.lua
@@ -2,6 +2,7 @@ local awful = require("awful")
 local wibox = require("wibox")
 
 local cpu_widget = require("ui.widgets.cpu-widget.cpu-widget")
+local assault = require("ui.widgets.battery.assault")
 -- {{{ Wibar
 
 -- Keyboard map indicator and switcher
@@ -86,6 +87,14 @@ screen.connect_signal("request::desktop_decoration", function(s)
                     step_spacing = 0,
                     color = '#434c5e'
                 }),4,4),
+                wibox.container.margin(assault({
+                    battery = "BAT0", -- 获取数据的电池ID
+                    adapter = "ADP0", -- 获取数据的交流适配器ID
+                    height = 13,
+                    critical_level = 0.15,
+                    critical_color = "#ff0000",
+                    charging_color = "#00ff00"
+                 }),4,4),
                 mytextclock,
                 s.mylayoutbox,
             },
